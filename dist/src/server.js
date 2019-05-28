dec049f3197ccb604e7ebf831fb4e5f493e09474
Mike Bush <mpbush@gmail.com>
2019-05-28 12:11:27 +0200

    Remove dist dir

--- dist/src/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const gitgovServer_1 = require("./gitgovServer");
-const server = gitgovServer_1.gitgovServer("repo");
-server.listen(25, () => console.log("SMTP server up"));
-//# sourceMappingURL=server.js.map
