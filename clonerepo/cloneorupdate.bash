ed3ad51e8519a26931bbc8daffff9acdadc67e62
Mike Bush <platy@njk.onl>
2021-12-15 10:21:26 +0100

    Ignore workdir for git update as no longer used

--- clonerepo/cloneorupdate.bash
+++ clonerepo/cloneorupdate.bash
@@ -6,15 +6,19 @@
 
 REPODIR=$1
 REMOTE=$2
-
-if [ ! -e $REPODIR -o ! "$(ls -A $REPODIR)" ]; then
-    echo cloning $REMOTE to $REPODIR
-    git clone $REMOTE $REPODIR
-elif [ "$(ls -A $REPODIR)" -a ! "$(git -C $REPODIR status -s)" -a "$(git -C $REPODIR remote get-url --all origin)" == "$REMOTE" ]; then
-    echo pulling $REMOTE to $REPODIR
-    git -C $REPODIR pull
+# TODO bare clone, pull may not work because workdir is dirty
+if [ ! -e "$REPODIR" ] || [ ! "$(ls -A "$REPODIR")" ]; then
+    echo "cloning $REMOTE to $REPODIR"
+    git clone "$REMOTE" "$REPODIR"
+elif [ "$(ls -A "$REPODIR")" ] && [ "$(git -C "$REPODIR" remote get-url --all origin)" == "$REMOTE" ]; then
+    echo "pulling $REMOTE to $REPODIR"
+    git -C "$REPODIR" pull
 else
     echo not clean
-    ls -A $REPODIR
+    echo "Contains $(ls -A "$REPODIR" | wc) files"
+    echo git status:
+    git -C "$REPODIR" status -s
+    echo git origin:
+    git -C "$REPODIR" remote get-url --all origin
     exit 1
 fi
